703199124f11cb2e9763c96b032bebe0d52a9db8
Ryaaha <vinmalikgithub@gmail.com>
2024-01-24 22:07:32 +0530

    initial commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,25 @@
+ const PortfolioData = [ //an array of objects
+    {
+        id: 1,
+        title: 'This website itself :)',
+        imgUrl: 'x',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link:'',
+    },
+    {
+        id: 2,
+        title: 'My Github',
+        imgUrl: 'x',
+        stack: ['x'],
+        link: 'https://github.com/Ryaaha',
+    },
+    {
+        id: 3,
+        title:'Text Editor (WIP)',
+        imgUrl: 'x',
+        stack: ['x'],
+        link: 'x',
+    }
+]
+
+export default PortfolioData;
