996f38cc4d6a426916ed2d8e67873c702c221b43
Ryaaha <vinmalikgithub@gmail.com>
2024-02-04 16:31:14 +0530

    Added images

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,21 +2,21 @@
     {
         id: 1,
         title: 'This website',
-        imgUrl: 'x',
+        imgUrl: '../../assets/images/wdev.png',
         stack: ['React', 'Tailwind', 'Vite'],
         link:'https://github.com/Ryaaha/portfolio',
     },
     {
         id: 2,
         title:'Text Editor (WIP)',
-        imgUrl: 'x',
+        imgUrl: '../../assets/images/editor.png',
         stack: ['C / C++'],
         link: 'x',
     },
     {
         id: 3,
         title:'URL Tracker (WIP)',
-        imgUrl: 'x',
+        imgUrl: '../../assets/images/clipboard.png',
         stack: ['Python'],
         link: 'x',
     }
